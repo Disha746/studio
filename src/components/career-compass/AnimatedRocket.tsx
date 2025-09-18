51b76d4559d8f23e916b8d7394b44098f7ca1cff
Disha Gangwani <gangwanidisha144@gmail.com>
2025-09-18 17:24:30 +0000

    Try fixing this error: `Build Error: Export motion doesn't exist in targ

--- src/components/career-compass/AnimatedRocket.tsx
+++ src/components/career-compass/AnimatedRocket.tsx
@@ -1,7 +1,8 @@
 
 "use client";
 
-import { motion, useAnimation, type Variants } from "motion";
+import { useAnimation, type Variants, animate } from "motion";
+import { useEffect, useRef } from "react";
 
 const variants: Variants = {
   normal: {
@@ -58,7 +59,26 @@ const AnimatedRocket = ({
   className,
   ...props
 }: RocketProps) => {
-  const controls = useAnimation();
+  const svgRef = useRef<SVGSVGElement>(null);
+  const firePathRef = useRef<SVGPathElement>(null);
+
+  const handleMouseEnter = () => {
+    if (svgRef.current) {
+        animate(svgRef.current, variants.animate.x ? variants.animate : { x: variants.animate.x, y: variants.animate.y }, variants.animate.transition);
+    }
+    if (firePathRef.current) {
+        animate(firePathRef.current, fireVariants.animate.d ? fireVariants.animate : { d: fireVariants.animate.d }, fireVariants.animate.transition);
+    }
+  };
+
+  const handleMouseLeave = () => {
+      if (svgRef.current) {
+        animate(svgRef.current, variants.normal, { duration: 0.5 });
+    }
+    if (firePathRef.current) {
+        animate(firePathRef.current, fireVariants.normal, { duration: 0.5 });
+    }
+  };
 
   return (
     <div
@@ -70,10 +90,11 @@ const AnimatedRocket = ({
         justifyContent: "center",
       }}
       className={className}
-      onMouseEnter={() => controls.start("animate")}
-      onMouseLeave={() => controls.start("normal")}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <motion.svg
+      <svg
+        ref={svgRef}
         xmlns="http://www.w3.org/2000/svg"
         width={width}
         height={height}
@@ -83,19 +104,16 @@ const AnimatedRocket = ({
         strokeWidth={strokeWidth}
         strokeLinecap="round"
         strokeLinejoin="round"
-        variants={variants}
-        animate={controls}
         {...props}
       >
-        <motion.path
+        <path
+          ref={firePathRef}
           d="M4.5 16.5c-1.5 1.26-2 5-2 5s3.74-.5 5-2c.71-.84.7-2.13-.09-2.91a2.18 2.18 0 0 0-2.91-.09z"
-          variants={fireVariants}
-          animate={controls}
         />
         <path d="m12 15-3-3a22 22 0 0 1 2-3.95A12.88 12.88 0 0 1 22 2c0 2.72-.78 7.5-6 11a22.35 22.35 0 0 1-4 2z" />
         <path d="M9 12H4s.55-3.03 2-4c1.62-1.08 5 0 5 0" />
         <path d="M12 15v5s3.03-.55 4-2c1.08-1.62 0-5 0-5" />
-      </motion.svg>
+      </svg>
     </div>
   );
 };
